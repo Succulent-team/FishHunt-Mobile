30222081fe6ebded2dfdfd37d088f417e8591923
matv864 <matv864@gmail.com>
2023-03-30 15:25:31 +1000

    camera
    camera

--- app/src/main/java/com/example/fishhunt/CameraActivity.kt
+++ app/src/main/java/com/example/fishhunt/CameraActivity.kt
@@ -1,7 +1,13 @@
 package com.example.fishhunt
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class CameraActivity : AppCompatActivity() {
@@ -10,12 +16,51 @@ class CameraActivity : AppCompatActivity() {
         const val NAME_OF_FISH = "BAD_TEXT"
 
     }
+    private val REQUEST_TAKE_PHOTO = 1
+
+    private lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_camera)
+
         some = findViewById(R.id.some)
         val count = intent.getStringExtra(NAME_OF_FISH)
-        some.text = count.toString()
+
+
+        imageView = findViewById(R.id.camera_view)
+        val button: Button = findViewById(R.id.take_foto)
+
+        button.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            // Фотка сделана, извлекаем миниатюру картинки
+            val thumbnailBitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(thumbnailBitmap)
+        }
+
+        // Другой вариант с применением when
+        /*when(requestCode){
+            REQUEST_TAKE_PHOTO ->{
+                if(resultCode == Activity.RESULT_OK && data !== null){
+                    imageView.setImageBitmap(data.extras?.get("data") as Bitmap)
+                }
+            }
+            else ->{
+                Toast.makeText(this, "Wrong request code", Toast.LENGTH_SHORT).show()
+            }
+        }*/
     }
 }
