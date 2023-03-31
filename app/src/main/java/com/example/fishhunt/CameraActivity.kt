a8d7bc45e80ad4805d26b135dd050f9b967a2685
matv864 <matv864@gmail.com>
2023-03-31 17:34:06 +1000

    Network
    Ruslan Kamalov is the best

--- app/src/main/java/com/example/fishhunt/CameraActivity.kt
+++ app/src/main/java/com/example/fishhunt/CameraActivity.kt
@@ -8,10 +8,9 @@ import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 
 class CameraActivity : AppCompatActivity() {
-    private lateinit var some: TextView
+//    private lateinit var some: TextView
     companion object {
         const val NAME_OF_FISH = "BAD_TEXT"
 
@@ -25,8 +24,8 @@ class CameraActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_camera)
 
-        some = findViewById(R.id.some)
-        val count = intent.getStringExtra(NAME_OF_FISH)
+//        some = findViewById(R.id.some)
+//        val count = intent.getStringExtra(NAME_OF_FISH)
 
 
         imageView = findViewById(R.id.camera_view)
--- app/src/main/java/com/example/fishhunt/FishHuntActivity.kt
+++ app/src/main/java/com/example/fishhunt/FishHuntActivity.kt
@@ -4,15 +4,29 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
 
 class FishHuntActivity : AppCompatActivity() {
-    val name_of_fish = "517"
+    open val name_of_fish = "fish"
+    private lateinit var name_target: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fish_hunt)
+        name_target = findViewById(R.id.name_of_target)
+        name_target.text = "bvrenhdjvrnm"
+
+        val url = "http://10.50.16.163:5000"
+        web_func(url)
+
     }
+
     fun back_to_menu(view: View){
         val randomIntent = Intent(this, MainActivity2::class.java)
         startActivity(randomIntent)
@@ -23,5 +37,18 @@ class FishHuntActivity : AppCompatActivity() {
         randomIntent.putExtra(CameraActivity.NAME_OF_FISH, name_of_fish)
         startActivity(randomIntent)
     }
+
+    private fun web_func(url: String) {
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                name_target.text = response.toString()
+            },
+            Response.ErrorListener { name_target.text = "That didn't work!" })
+
+        queue.add(stringRequest)
+    }
+
 
 }
