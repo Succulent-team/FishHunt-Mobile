1999018bfc873d6753af5eee6b87309fbeb767d1
matv864 <matv864@gmail.com>
2023-04-03 18:47:34 +1000

    fix
    fix

--- app/src/main/java/com/example/fishhunt/CameraActivity.kt
+++ app/src/main/java/com/example/fishhunt/CameraActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 
@@ -15,6 +16,7 @@ class CameraActivity : AppCompatActivity() {
     private lateinit var imageView: ImageView
     private lateinit var take_photo: Button
     private lateinit var go_check_photo: Button
+    private var now_id_fish: String? = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,9 +33,16 @@ class CameraActivity : AppCompatActivity() {
             }
         }
 
-
-
+        val arguments = getIntent().getExtras()
+        now_id_fish = arguments?.getString("now_id_fish")
     }
+
+    fun back_to_info_fish(view : View){
+        val intent = Intent(this, FishHuntActivity::class.java)
+        intent.putExtra("now_id_fish", now_id_fish)
+        startActivity(intent)
+    }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
